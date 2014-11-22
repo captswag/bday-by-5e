816ca709d21bd9bc0e71ed4e8d4701219c7ca7aa
Anjith Sasindran <iamanjithsasindran@gmail.com>
2014-11-22 14:14:09 +0530

    Updated MainActivity with custom SearchView and Activity layout. Added SearchView filtering in ListView

--- gen/com/anjith/bdayby5e/R.java
+++ gen/com/anjith/bdayby5e/R.java
@@ -9,9 +9,13 @@ package com.anjith.bdayby5e;
 
 public final class R {
     public static final class array {
-        public static final int names=0x7f050000;
+        public static final int bday=0x7f070001;
+        public static final int names=0x7f070000;
     }
     public static final class attr {
+    }
+    public static final class color {
+        public static final int bdaysearc_color=0x7f040000;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -21,8 +25,8 @@ public final class R {
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
          */
-        public static final int activity_horizontal_margin=0x7f030000;
-        public static final int activity_vertical_margin=0x7f030001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int ab_background_textured_bdayaction=0x7f020000;
@@ -31,58 +35,67 @@ public final class R {
         public static final int ab_stacked_solid_bdayaction=0x7f020003;
         public static final int ab_texture_tile_bdayaction=0x7f020004;
         public static final int ab_transparent_bdayaction=0x7f020005;
-        public static final int btn_cab_done_bdayaction=0x7f020006;
-        public static final int btn_cab_done_default_bdayaction=0x7f020007;
-        public static final int btn_cab_done_focused_bdayaction=0x7f020008;
-        public static final int btn_cab_done_pressed_bdayaction=0x7f020009;
-        public static final int cab_background_bottom_bdayaction=0x7f02000a;
-        public static final int cab_background_top_bdayaction=0x7f02000b;
-        public static final int ic_action_collapse=0x7f02000c;
-        public static final int ic_action_person=0x7f02000d;
-        public static final int ic_action_search=0x7f02000e;
-        public static final int ic_launcher=0x7f02000f;
-        public static final int list_focused_bdayaction=0x7f020010;
-        public static final int list_pressed_bdayaction=0x7f020011;
-        public static final int menu_dropdown_panel_bdayaction=0x7f020012;
-        public static final int progress_bg_bdayaction=0x7f020013;
-        public static final int progress_horizontal_bdayaction=0x7f020014;
-        public static final int progress_primary_bdayaction=0x7f020015;
-        public static final int progress_secondary_bdayaction=0x7f020016;
-        public static final int selectable_background_bdayaction=0x7f020017;
-        public static final int spinner_ab_default_bdayaction=0x7f020018;
-        public static final int spinner_ab_disabled_bdayaction=0x7f020019;
-        public static final int spinner_ab_focused_bdayaction=0x7f02001a;
-        public static final int spinner_ab_pressed_bdayaction=0x7f02001b;
-        public static final int spinner_background_ab_bdayaction=0x7f02001c;
-        public static final int tab_indicator_ab_bdayaction=0x7f02001d;
-        public static final int tab_selected_bdayaction=0x7f02001e;
-        public static final int tab_selected_focused_bdayaction=0x7f02001f;
-        public static final int tab_selected_pressed_bdayaction=0x7f020020;
-        public static final int tab_unselected_bdayaction=0x7f020021;
-        public static final int tab_unselected_focused_bdayaction=0x7f020022;
-        public static final int tab_unselected_pressed_bdayaction=0x7f020023;
+        public static final int bdaysearc_edit_text_holo_light=0x7f020006;
+        public static final int bdaysearc_textfield_activated_holo_light=0x7f020007;
+        public static final int bdaysearc_textfield_default_holo_light=0x7f020008;
+        public static final int bdaysearc_textfield_disabled_focused_holo_light=0x7f020009;
+        public static final int bdaysearc_textfield_disabled_holo_light=0x7f02000a;
+        public static final int bdaysearc_textfield_focused_holo_light=0x7f02000b;
+        public static final int btn_cab_done_bdayaction=0x7f02000c;
+        public static final int btn_cab_done_default_bdayaction=0x7f02000d;
+        public static final int btn_cab_done_focused_bdayaction=0x7f02000e;
+        public static final int btn_cab_done_pressed_bdayaction=0x7f02000f;
+        public static final int cab_background_bottom_bdayaction=0x7f020010;
+        public static final int cab_background_top_bdayaction=0x7f020011;
+        public static final int ic_action_collapse=0x7f020012;
+        public static final int ic_action_person=0x7f020013;
+        public static final int ic_action_search=0x7f020014;
+        public static final int ic_launcher=0x7f020015;
+        public static final int list_focused_bdayaction=0x7f020016;
+        public static final int list_pressed_bdayaction=0x7f020017;
+        public static final int menu_dropdown_panel_bdayaction=0x7f020018;
+        public static final int progress_bg_bdayaction=0x7f020019;
+        public static final int progress_horizontal_bdayaction=0x7f02001a;
+        public static final int progress_primary_bdayaction=0x7f02001b;
+        public static final int progress_secondary_bdayaction=0x7f02001c;
+        public static final int selectable_background_bdayaction=0x7f02001d;
+        public static final int spinner_ab_default_bdayaction=0x7f02001e;
+        public static final int spinner_ab_disabled_bdayaction=0x7f02001f;
+        public static final int spinner_ab_focused_bdayaction=0x7f020020;
+        public static final int spinner_ab_pressed_bdayaction=0x7f020021;
+        public static final int spinner_background_ab_bdayaction=0x7f020022;
+        public static final int tab_indicator_ab_bdayaction=0x7f020023;
+        public static final int tab_selected_bdayaction=0x7f020024;
+        public static final int tab_selected_focused_bdayaction=0x7f020025;
+        public static final int tab_selected_pressed_bdayaction=0x7f020026;
+        public static final int tab_unselected_bdayaction=0x7f020027;
+        public static final int tab_unselected_focused_bdayaction=0x7f020028;
+        public static final int tab_unselected_pressed_bdayaction=0x7f020029;
     }
     public static final class id {
-        public static final int about=0x7f080002;
-        public static final int search=0x7f080000;
-        public static final int upcoming=0x7f080001;
+        public static final int about=0x7f0a0002;
+        public static final int search=0x7f0a0000;
+        public static final int upcoming=0x7f0a0001;
+    }
+    public static final class layout {
+        public static final int activity_main=0x7f030000;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f090000;
     }
     public static final class string {
-        public static final int about=0x7f040005;
-        public static final int action_settings=0x7f040002;
-        public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
-        public static final int search=0x7f040003;
-        public static final int upcoming=0x7f040004;
+        public static final int about=0x7f060005;
+        public static final int action_settings=0x7f060002;
+        public static final int app_name=0x7f060000;
+        public static final int hello_world=0x7f060001;
+        public static final int search=0x7f060003;
+        public static final int upcoming=0x7f060004;
     }
     public static final class style {
-        public static final int ActionBar_Solid_Bdayaction=0x7f060003;
-        public static final int ActionBar_Transparent_Bdayaction=0x7f060004;
-        public static final int ActionBarTabStyle_Bdayaction=0x7f060007;
-        public static final int ActionButton_CloseMode_Bdayaction=0x7f06000a;
+        public static final int ActionBar_Solid_Bdayaction=0x7f080003;
+        public static final int ActionBar_Transparent_Bdayaction=0x7f080004;
+        public static final int ActionBarTabStyle_Bdayaction=0x7f080007;
+        public static final int ActionButton_CloseMode_Bdayaction=0x7f08000a;
         /** 
         Base application theme, dependent on API level. This theme is replaced
         by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
@@ -104,18 +117,20 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
-        public static final int DropDownListView_Bdayaction=0x7f060006;
-        public static final int DropDownNav_Bdayaction=0x7f060008;
-        public static final int PopupMenu_Bdayaction=0x7f060005;
-        public static final int ProgressBar_Bdayaction=0x7f060009;
-        public static final int Theme_Bdayaction=0x7f060002;
+        public static final int AppTheme=0x7f080001;
+        public static final int DropDownListView_Bdayaction=0x7f080006;
+        public static final int DropDownNav_Bdayaction=0x7f080008;
+        public static final int PopupMenu_Bdayaction=0x7f080005;
+        public static final int ProgressBar_Bdayaction=0x7f080009;
+        public static final int Theme_Bdayaction=0x7f080002;
         /**  this style is only referenced in a Light.DarkActionBar based theme 
          */
-        public static final int Theme_Bdayaction_Widget=0x7f06000b;
+        public static final int Theme_Bdayaction_Widget=0x7f08000b;
+        public static final int _bdaysearc=0x7f08000d;
+        public static final int bdaysearc=0x7f08000c;
     }
 }
--- src/com/anjith/bdayby5e/MainActivity.java
+++ src/com/anjith/bdayby5e/MainActivity.java
@@ -2,11 +2,17 @@ package com.anjith.bdayby5e;
 
 import android.app.ListActivity;
 import android.content.res.Resources;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.SearchView;
+import android.widget.SearchView.OnQueryTextListener;
 import android.widget.Toast;
 
 
@@ -14,26 +20,77 @@ public class MainActivity extends ListActivity {
 
 	SearchView searchView;
 	Resources resources;
+	
+	//Strings to store names and bdays of friends
 	String names[];
+	String bdays[];
+	
+	ListView listView;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
         
         resources = getResources();
         
+        //Get the values from string resource
         names = resources.getStringArray(R.array.names);
+        bdays = resources.getStringArray(R.array.bday);
         
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, names);
+        	
         setListAdapter(adapter);
+        
+        listView = getListView();
+        listView.setTextFilterEnabled(true);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
+    	int searchViewId;
+    	
         getMenuInflater().inflate(R.menu.main, menu);
+        
         searchView = (SearchView)menu.findItem(R.id.search).getActionView();
-        return true;
+        
+        //Get the SearchPlate id
+        int searchPlateId = searchView.getContext().getResources().getIdentifier("android:id/search_plate", null, null);
+        
+        //Get the SearchPlate view and set it's background to transparent to remove blue EditText default drawable
+        View searchPlate = searchView.findViewById(searchPlateId);
+        searchPlate.setBackgroundColor(Color.TRANSPARENT);
+        
+        //Get the id of SearchView's EditText
+        searchViewId = searchView.getContext().getResources().getIdentifier("android:id/search_src_text", null, null);
+        
+        //Get the EditText in SearchView using the above id
+        EditText searchEdit = (EditText) searchView.findViewById(searchViewId);
+        searchEdit.setBackgroundResource(R.drawable.bdaysearc_edit_text_holo_light);
+        
+        searchView.setOnQueryTextListener(new OnQueryTextListener() {
+			
+			@Override
+			public boolean onQueryTextSubmit(String arg0) {
+				// TODO Auto-generated method stub
+				return false;
+			}
+			
+			@Override
+			public boolean onQueryTextChange(String arg0) {
+				// TODO Auto-generated method stub
+				if (TextUtils.isEmpty(arg0)) {
+					listView.clearTextFilter();
+				} else {
+					listView.setFilterText(arg0.toString());
+				}
+				return true; //of onQueryTextChange(String arg0)
+			}
+		});
+        
+        return true; //of onCreateOptionsMenu()
     }
 
     @Override
